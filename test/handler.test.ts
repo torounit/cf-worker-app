88d33374f3e5fef120de4e8f95385e548681845d
Hiroshi Urabe <mail@torounit.com>
2021-07-31 16:38:08 +0900

    init

---
+++ test/handler.test.ts
@@ -0,0 +1,16 @@
+import { handleRequest } from '../src/handler'
+import makeServiceWorkerEnv from 'service-worker-mock'
+
+describe('handle', () => {
+  beforeEach(() => {
+    Object.assign(globalThis, makeServiceWorkerEnv())
+    jest.resetModules()
+  })
+
+  test('handle GET', async () => {
+    const result = await handleRequest(new Request('/', { method: 'GET' }))
+    expect(result.status).toEqual(200)
+    const text = await result.text()
+    expect(text).toEqual('request method: GET')
+  })
+})
